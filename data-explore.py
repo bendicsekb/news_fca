e7a2162e984e9047963fd842d602298651d70acd
M. Bendegúz Bendicsek <bendicsekb@gmail.com>
2022-12-12 15:24:39 +0100

    Make module + functions

--- data-explore.py
+++ data-explore.py
@@ -4,3 +4,28 @@ import pandas as pd
 #["Text"].apply(lambda e: len(e)).describe()
 pd.read_csv("data/BBC News Train.csv")
 # %%
+
+news = pd.read_csv("data/BBC News Train.csv")
+
+# create an enum with the categories in the dataset, with categories as object attributes
+categories = news["Category"].unique()
+category = type("Category", (), dict(zip(categories, range(len(categories)))))
+news["Category"] = news["Category"].apply(lambda e: category.__dict__[e])
+
+# %% 
+# create a new column 'short' where the valueis True if the text is shorter than the median length of the text
+median_length = news["Text"].apply(lambda e: len(e)).median()
+news["Short"] = news["Text"].apply(lambda e: len(e) < median_length)
+news["Long"] = news["Short"].__neg__()
+
+
+# %%
+# keep a sample of 30 of the dataset from each category
+sample = news.groupby("Category").apply(lambda e: e.sample(30, random_state=42))
+
+
+# %%
+print(sample['Short'].value_counts())
+print(sample['Category'].value_counts())
+sample.describe()
+# %%
---
+++ functions.py
@@ -0,0 +1,14 @@
+def make_sample(df, n=30, random_state=42):
+    return df.groupby("Category").apply(lambda e: e.sample(n, random_state=random_state))
+
+def create_short_long(df):
+    median_length = df["Text"].apply(lambda e: len(e)).median()
+    df["Short"] = df["Text"].apply(lambda e: len(e) < median_length)
+    df["Long"] = df["Short"].__neg__()
+    return df
+
+def extract_categories(df):
+    categories = df["Category"].unique()
+    category = type("Category", (), dict(zip(categories, range(len(categories)))))
+    df["Category"] = df["Category"].apply(lambda e: category.__dict__[e])
+    return df
